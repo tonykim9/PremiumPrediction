c16184375b4c5b35c85641214075e9ea6cdf9af3
first commit <Dohyun@sindohyeon-ui-MacBook-Pro.local>
2017-08-28 15:24:36 +0900

    premium_prediction 1.0

---
+++ project/premium_features.py
@@ -0,0 +1,5 @@
+#premium_features
+#This file will turn raw statistics into features that are fed
+#into the machine learning model
+#The data is combined from the premium_stats.py
+#created 28th August, 2017
---
+++ project/premium_stats.py
@@ -0,0 +1,4 @@
+#this file will call the data needed for logistic regression
+#the file called will be the 2016League.csv
+#later to be changed to handle dynamic data
+#created 28th August, 2017 by Shin Do Hyun
