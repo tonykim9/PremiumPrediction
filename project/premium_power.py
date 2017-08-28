367b0a7a9ad4875e8fd3f2f96696d9c1f77f441c
first commit <Dohyun@sindohyeon-ui-MacBook-Pro.local>
2017-08-28 15:27:13 +0900

    premium_prediction 1.0

---
+++ project/premium_power.py
@@ -0,0 +1,4 @@
+#this file computes a statistic over a number of teams who have played
+#against each other, creating a ranking
+#stems from the original power.py file
+#created by Shin Do Hyun on 28th August, 2017
